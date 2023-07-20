ee1d71c11910bffa2df098d9f209364f72d56a45
trantanthanh <trantanthanh@gmail.com>
2023-07-20 21:03:15 +0700

    add speed boost, use FindObjectOfType

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rg2d;
     [SerializeField] float torqueValueAmout = 60.0f;
+    [SerializeField] float boostSpeed = 20f;
+    [SerializeField] float normalSpeed = 15f;
+    [SerializeField] float lowSpeed = 10f;
+    SurfaceEffector2D surface2D;
     // Start is called before the first frame update
     void Start()
     {
         rg2d = GetComponent<Rigidbody2D>();
+        surface2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        checkRotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            surface2D.speed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            surface2D.speed = lowSpeed;
+        }
+        else
+        {
+            surface2D.speed = normalSpeed;
+        }
+    }
+
+    private void checkRotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
