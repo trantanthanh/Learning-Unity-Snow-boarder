fbf94d56ee3fc1e1b4d1fd220bdae69621229f24
trantanthanh <trantanthanh@gmail.com>
2023-07-20 12:58:38 +0700

    add crash effect, finish effect

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float timeReloadDelay = 1.0f;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground")
         {
             Debug.Log("Ouch !! hit my head");
+            Invoke("ReloadScene", timeReloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
-    
+
+    [SerializeField] float timeReloadDelay = 1.0f;
     /// <summary>
     /// Sent when another object enters a trigger collider attached to this
     /// object (2D physics only).
@@ -12,9 +13,15 @@ public class FinishLine : MonoBehaviour
     /// <param name="other">The other Collider2D involved in this collision.</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")    
+        if (other.tag == "Player")
         {
             Debug.Log("You're finished");
+            Invoke("ReloadScene", timeReloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
